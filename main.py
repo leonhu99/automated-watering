d3362d820e143bc49d821726b643d964e8cc9de8
Leon Hubert <hubertleon9@googlemail.com>
2025-02-01 19:28:46 +0100

    Refactor: Moved some functionality from main to util

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import requests
 import time
 import spidev
 from typing import List
@@ -6,53 +5,6 @@ from pump import Pump
 from sensor import Sensor
 from gpio_setup import GPIO_Setup
 from util import *
-
-
-def generate_sensor_data(sensor_list: List[Sensor]) -> any:
-    """Function that generates the sensor data for the webserver.
-
-    Parameters
-    ----------
-    sensor_list : List[Sensor]
-        A list of Sensor objects
-
-    Returns
-    -------
-    The generated data in the typical format (json string).
-    """
-    
-    data = []
-    for sensor in sensor_list:
-        data.append({
-            "sensor_id": sensor.id, 
-            "last_value": sensor.last_value,
-            "dry_value": sensor.dry_value,
-            "wet_value": sensor.wet_value,
-            "interval_size": sensor.interval_size
-        })
-    
-    return data
-
-
-def send_sensor_data(data: any, SERVER_URL: str) -> None:
-    """Function that sends the sensor data to the webserver.
-
-    Parameters
-    ----------
-    data : any
-        json string containing the sensor data
-    SERVER_URL : str
-        A string contianign the url of the server including ip address, port and route.
-
-    Returns
-    -------
-    None
-    """
-    
-    try:
-        requests.post(SERVER_URL, json = data)
-    except Exception as e:
-        print("Error while sending data! ", {e})
 
 
 def run() -> None:
