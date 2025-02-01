b4f88ecc25eec203d41639d477c03c5e14bc41d6
Leon Hubert <hubertleon9@googlemail.com>
2025-02-01 18:53:10 +0100

    Update: config-file is now read 1x instead of 2x in gpio-setup

--- gpio_setup.py
+++ gpio_setup.py
@@ -1,21 +1,14 @@
 import yaml
 import util
 import RPi.GPIO as GPIO
-from typing import List, Union
+from typing import List
 from pump import *
 from sensor import *
 
 
 class GPIO_Setup:
 
-    def __instantiate_pumps(pump_list: List[Pump]) -> None:
-        # load pump_data from config/config.yaml
-        try:
-            with open('config/config.yaml', 'r') as config_file:
-                pump_data = yaml.safe_load(config_file)
-        except FileNotFoundError:
-            print("Config file cannot be found!")
-
+    def __instantiate_pumps(pump_data: any, pump_list: List[Pump]) -> None:
         # create pumps from read data
         for pump in pump_data['pumps']:
             temp_pump = Pump(id = pump['id'], pin = pump['pin'], pump_rate = pump['pump_rate'])
@@ -28,14 +21,7 @@ class GPIO_Setup:
             GPIO.output(temp_pump.pin, GPIO.HIGH) 
 
 
-    def __instantiate_sensors(sensor_list: List[Sensor]) -> None:
-        # load sensor_data from config/config.yaml
-        try:
-            with open('config/config.yaml', 'r') as config_file:
-                sensor_data = yaml.safe_load(config_file)
-        except FileNotFoundError:
-            print("Config file cannot be found!")
-
+    def __instantiate_sensors(sensor_data: any, sensor_list: List[Sensor]) -> None:
         # create sensors from read data
         for sensor in sensor_data['sensors']:
             temp_sensor = Sensor(id = sensor['id'], description = sensor['description'], dry_value = sensor['dry_value'], wet_value = sensor['wet_value'], last_value = 0)
@@ -45,7 +31,20 @@ class GPIO_Setup:
         util.init_last_values(sensor_list)
 
 
+    def __read_IO_data() -> None:
+        IO_DATA = ""
+        try:
+            with open('config/config.yaml', 'r') as config_file:
+                IO_DATA = yaml.safe_load(config_file)
+        except FileNotFoundError:
+            print("Config file cannot be found!") 
+
+        return IO_DATA      
+
+
     def configure(board_mode: str, pump_list: List[Pump], sensor_list: List[Sensor]) -> None:
         GPIO.setmode(GPIO.BOARD) if board_mode == "BOARD" else GPIO.setmode(GPIO.BCM)
-        GPIO_Setup.__instantiate_pumps(pump_list)
-        GPIO_Setup.__instantiate_sensors(sensor_list)
+
+        IO_DATA = GPIO_Setup.__read_IO_data()
+        GPIO_Setup.__instantiate_pumps(IO_DATA, pump_list)
+        GPIO_Setup.__instantiate_sensors(IO_DATA, sensor_list)
