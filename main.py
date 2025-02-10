a0db08050527a994d94ea4c831920e8f3cb9c619
Leon Hubert <hubertleon9@googlemail.com>
2025-02-10 22:17:31 +0100

    Feature: Added option to enable Day-Night-Schedule

--- main.py
+++ main.py
@@ -17,12 +17,15 @@ def run() -> None:
 
     # load general configuration
     config = get_configuration()
-    AMOUNT_OF_WATER = config[0]
+    AMOUNT_OF_WATER: float = config[0]
     INTERVAL_TIME: float = config[1]
     USE_WEBSERVER: bool = config[2]
     SERVER_URL: str = f'http://{config[3]}:{config[4]}/api/sensors'
     BOARD_MODE: str = config[5]
     MEASUREMENT_SAMPLES: int = config[6]
+    USE_WATERING_SCHEDULE: bool = config[7]
+    WATERING_WINDOW_START: datetime.time = config[8]
+    WATERING_WINDOW_END: datetime.time = config[9]
 
     # SPI configuration
     SPI1 = spidev.SpiDev(0, 0) # CE0 for MCP3008#1 (sensors 1-6)
@@ -42,14 +45,27 @@ def run() -> None:
 
     while(True):
         read_analog_sensors(sensor_list, SPI1, SPI2, MEASUREMENT_SAMPLES)
-        water_plants(pump_list, sensor_list, AMOUNT_OF_WATER)
+
+        if USE_WATERING_SCHEDULE:
+            if WATERING_WINDOW_START <= datetime.now().time() <= WATERING_WINDOW_END:
+                water_plants(pump_list, sensor_list, AMOUNT_OF_WATER)
+            else:
+                print(f'Not in watering window - skipping...')
 
         if USE_WEBSERVER:
             # send sensor data to server
             data = generate_sensor_data(sensor_list)
             send_sensor_data(data, SERVER_URL)
 
-        print(f'Sleeping.. Next measuring will be in {INTERVAL_TIME} seconds!')
+        if INTERVAL_TIME >= 86400:
+            print(f'Sleeping.. Next measuring will be in {str(round(INTERVAL_TIME/86400, 2))} days!')
+        elif INTERVAL_TIME >= 21600:
+            print(f'Sleeping.. Next measuring will be in {str(round(INTERVAL_TIME/3600, 2))} hours!')
+        elif INTERVAL_TIME >= 1800:
+            print(f'Sleeping.. Next measuring will be in {str(round(INTERVAL_TIME/60, 2))} minutes!')
+        else:
+            print(f'Sleeping.. Next measuring will be in {str(INTERVAL_TIME)} seconds!')
+
         time.sleep(INTERVAL_TIME)
 
 
